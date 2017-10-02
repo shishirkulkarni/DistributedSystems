972b77938759f8bd0dab57d561ad148c530364c2
Shishir Kulkarni <shishir.kulkarni.2307@gmail.com>
2017-10-02 15:48:36 -0700

    cs249: implementaed vector clocks

---
+++ Homework 5/Algorithm.java
@@ -0,0 +1,32 @@
+package edu.sjsu.cs249.three;
+
+public class Algorithm {
+	int noOfProcessors;
+	Processor p0, p1, p2; // Assume there are three processors.
+
+	public Algorithm() {
+		super();
+		this.noOfProcessors = 3;
+		// TODO : initialize all the processors
+		p0 = new Processor(0, 3, 0, 2);
+		p1 = new Processor(1, 3, 0, 1);
+		p2 = new Processor(2, 3, 0, 3);
+		p0.setExecutionPlan(p0, p1, p0, p2, p0, p0);
+		p1.setExecutionPlan(p1, p0, p1, p1, p2, p0, p1);
+		p2.setExecutionPlan(p2, p0, p2, p1, p2);
+	}
+	
+	public void execute() {
+		p0.start();
+		p1.start();
+		p2.start();
+		p0.join();
+		p1.join();
+		p2.join();
+		
+		System.out.println();
+		System.out.println("+-------------------------------------------+");
+		System.out.println("| P0: " + p0.getVc() + " P1: " + p1.getVc() + " P2: " + p2.getVc() + " |");
+		System.out.println("+-------------------------------------------+");
+	}
+}
---
+++ Homework 5/Processor.java
@@ -0,0 +1,103 @@
+package edu.sjsu.cs249.three;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Performs all the processor related tasks
+ * @author Sample
+ * @version 1.0
+ *
+ */
+public class Processor implements Observer {
+	//TODo : add appropriate visibility indicators to each member variable
+    private Buffer messageBuffer ;
+    private Integer id ;
+    //TODO: Think through when would you need a list of vector clocks
+    private VectorClock vc; //This is the current vector clock 
+    private Clock logicalClock;
+    private Processor []executionPlan;
+    private int pc; //Program counter
+
+    /**
+     * Initializes the processor with id, children and unexplored lists. Adds himself in the observers list.
+     * @param id of the processor
+     */
+    public Processor(int id, int totalProcesors, int jiffy, int interval) {
+        messageBuffer = new Buffer();
+        
+        this.id = id; 
+        
+        messageBuffer.addObserver(this);
+        
+        this.logicalClock = new Clock(jiffy, interval);
+        this.logicalClock.addObserver(this);
+        this.vc = new VectorClock(totalProcesors);
+        pc = 0;
+    }
+    
+    //proxy methods for threads
+    public void start() {
+    	this.logicalClock.start();
+    }
+    
+    public void join() {
+    	this.logicalClock.join();
+    }
+   
+    
+    /**
+     * Overloaded method, called with single argument
+     * This method will add a message to this processors buffer.
+     * Other processors will invoke this method to send a message to this Processor
+     * @param message Message to be sent
+     */
+    public void sendMessgeToMyBuffer(Message message){
+    	messageBuffer.setMessage(message);
+    }
+
+
+    /**
+     * Gets called when a node receives a message in it buffer
+     * Processes the message received in the buffer
+     */
+    public void update(Observable observable, Object arg) {
+    	//If a clock tick
+    	if(observable == this.logicalClock) {
+    		try {
+    			Processor k = executionPlan[pc++];
+    			if(k == this) {
+					this.vc.updateAt(id, (Integer)arg);
+					System.out.println("[Processor = P" + this + "] [COMPUTATION] [VC = " + this.vc + "]");
+    			} else {
+    				this.vc.updateAt(id, (Integer)arg);
+    				System.out.println("[Processor = P" + this + "] [SEND] [VC =  " + this.vc + "]");
+    				k.sendMessgeToMyBuffer(new Message(MessageType.SEND, vc));
+    			}
+    		} catch (ArrayIndexOutOfBoundsException e) { // Done with the Execution
+				this.logicalClock.stop();
+			}
+    	}
+    	
+    	//If a message
+    	if(observable == messageBuffer) {
+    		System.out.println("[RECIEVE] [VC = " + messageBuffer.getMessage().getVc() + "]");
+    		vc.updateVectorClock(messageBuffer.getMessage().getVc());
+    		System.out.println("[RECIEVE] [VC = " + vc + "]");
+    	}
+    }
+    
+    public void setExecutionPlan(Processor... executionPlan) {
+		this.executionPlan = executionPlan;
+	}
+    
+    public VectorClock getVc() {
+		return vc;
+	}
+    
+    @Override
+    public String toString() {
+    	// TODO Auto-generated method stub
+    	return "" + id;
+    }
+}
